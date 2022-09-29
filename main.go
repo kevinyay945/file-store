e37f467314e42cbd7835fef7bcb34623a595afe8
kevinyay945 <kevinyay945@gmail.com>
2022-09-29 23:26:42 +0800

    feat: add echo server for hello world

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
