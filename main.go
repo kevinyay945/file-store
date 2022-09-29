bfb7a68221b229f6d0e860953f0ed87a90bc6cca
kevinyay945 <kevinyay945@gmail.com>
2022-09-30 05:54:37 +0800

    feat: add hotfix post api

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import (
 	"net/http"
 )
 
+type MyResponse struct {
+	Data struct {
+		Link string `json:"link"`
+	} `json:"data"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.POST("/", func(c echo.Context) error {
+		res := new(MyResponse)
+		res.Data.Link = "https://xxxxxx.com/xxx.png"
+		return c.JSON(http.StatusOK, res)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
