58002c487f757313efc0dcdeacf338eb418526d6
kevinyay945 <kevinyay945@gmail.com>
2022-10-10 15:13:14 +0800

    implement http client for post and multiplepart file

--- utils/http-client/client.go
+++ utils/http-client/client.go
@@ -1,6 +1,10 @@
 package httpClient
 
-import "github.com/go-resty/resty/v2"
+import (
+	"github.com/go-resty/resty/v2"
+	"io"
+	"net/url"
+)
 
 type Response struct {
 	Body []byte
@@ -9,11 +13,53 @@ type Response struct {
 type IHttpClient interface {
 	Get(url string) (resp Response, err error)
 	SetAuthToken(s string) IHttpClient
+	Post(s string) (resp Response, err error)
+	SetQueryParam(key string, value string) IHttpClient
+	SetFileReader(param string, fileName string, reader io.Reader) IHttpClient
+}
+
+type MultipartFile struct {
+	Param    string
+	FileName string
+	Reader   io.Reader
 }
 
 type HttpClient struct {
-	resty     *resty.Client
-	authToken string
+	resty          *resty.Client
+	authToken      string
+	queryParam     url.Values
+	multipartFiles []MultipartFile
+}
+
+func (h *HttpClient) SetFileReader(param string, fileName string, reader io.Reader) IHttpClient {
+	h.multipartFiles = append(h.multipartFiles, MultipartFile{
+		Param:    param,
+		FileName: fileName,
+		Reader:   reader,
+	})
+	return h
+}
+
+func (h *HttpClient) SetQueryParam(key string, value string) IHttpClient {
+	h.queryParam.Set(key, value)
+	return h
+}
+
+func (h *HttpClient) Post(s string) (resp Response, err error) {
+	r := h.resty.R()
+	if h.authToken != "" {
+		r.SetAuthToken(h.authToken)
+	}
+	r.SetQueryParamsFromValues(h.queryParam)
+	for _, file := range h.multipartFiles {
+		r = r.SetFileReader(file.Param, file.FileName, file.Reader)
+	}
+	post, err := r.Post(s)
+	if err != nil {
+		return
+	}
+	resp = toResponse(post)
+	return
 }
 
 func (h *HttpClient) SetAuthToken(s string) IHttpClient {
@@ -24,7 +70,9 @@ func (h *HttpClient) SetAuthToken(s string) IHttpClient {
 func NewHttpClient() IHttpClient {
 	_resty := resty.New()
 	return &HttpClient{
-		resty: _resty,
+		resty:          _resty,
+		queryParam:     url.Values{},
+		multipartFiles: []MultipartFile{},
 	}
 }
 
@@ -33,7 +81,13 @@ func (h *HttpClient) Get(url string) (resp Response, err error) {
 	if h.authToken != "" {
 		r.SetAuthToken(h.authToken)
 	}
+	r = r.SetQueryParamsFromValues(h.queryParam)
 	response, err := r.Get(url)
+	resp = toResponse(response)
+	return
+}
+
+func toResponse(response *resty.Response) (resp Response) {
 	resp.Body = response.Body()
 	return
 }
--- utils/http-client/client_test.go
+++ utils/http-client/client_test.go
@@ -1,10 +1,12 @@
 package httpClient
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/joho/godotenv"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
 	"os"
 )
 
@@ -50,4 +52,58 @@ var _ = Describe("Http Client", func() {
 		Expect(result.Result).Should(Equal(0))
 
 	})
+	It("Get With Query Param", func() {
+		_client.SetQueryParam("postId", "1")
+		resp, err := _client.Get("https://jsonplaceholder.typicode.com/comments")
+		var result []struct {
+			PostId int    `json:"postId"`
+			Id     int    `json:"id"`
+			Name   string `json:"name"`
+			Email  string `json:"email"`
+			Body   string `json:"body"`
+		}
+		err = json.Unmarshal(resp.Body, &result)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(len(result)).Should(Equal(5))
+	})
+	It("Post With Query Param and auth token", func() {
+		profileImgBytes, _ := ioutil.ReadFile("./wakuwaku.jpeg")
+		_client.SetAuthToken(os.Getenv("PCLOUD_ACCESS_TOKEN"))
+		_client.SetQueryParam("folderid", "14868593431")
+		_client.SetFileReader("filename", "test_for_wakuwaku.jpeg", bytes.NewReader(profileImgBytes))
+		resp, err := _client.Post("https://api.pcloud.com/uploadfile")
+		Expect(err).ShouldNot(HaveOccurred())
+		result := new(struct {
+			Result   int `json:"result"`
+			Metadata []struct {
+				Name           string  `json:"name"`
+				Created        string  `json:"created"`
+				Thumb          bool    `json:"thumb"`
+				Modified       string  `json:"modified"`
+				Isfolder       bool    `json:"isfolder"`
+				Height         int     `json:"height"`
+				Fileid         int64   `json:"fileid"`
+				Width          int     `json:"width"`
+				Hash           float64 `json:"hash"`
+				Path           string  `json:"path"`
+				Category       int     `json:"category"`
+				Id             string  `json:"id"`
+				Isshared       bool    `json:"isshared"`
+				Ismine         bool    `json:"ismine"`
+				Size           int     `json:"size"`
+				Parentfolderid int64   `json:"parentfolderid"`
+				Contenttype    string  `json:"contenttype"`
+				Icon           string  `json:"icon"`
+			} `json:"metadata"`
+			Checksums []struct {
+				Sha1 string `json:"sha1"`
+				Md5  string `json:"md5"`
+			} `json:"checksums"`
+			Fileids []int64 `json:"fileids"`
+		})
+		err = json.Unmarshal(resp.Body, &result)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(result.Result).Should(Equal(0))
+	})
+
 })
