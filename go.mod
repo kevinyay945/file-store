07bed58206953e731de01c2a28b0e25cc754c04c
kevinyay945 <kevinyay945@gmail.com>
2023-02-03 22:01:21 +0800

    fix: test case fix

--- go.mod
+++ go.mod
@@ -12,11 +12,11 @@ require (
 	github.com/onsi/gomega v1.20.2
 	github.com/sirupsen/logrus v1.9.0
 	golang.org/x/net v0.0.0-20220909164309-bea034e7d591
-	google.golang.org/api v0.101.0
+	google.golang.org/api v0.47.0
 )
 
 require (
-	cloud.google.com/go/compute v1.10.0 // indirect
+	cloud.google.com/go v0.81.0 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/andybalholm/brotli v1.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -26,9 +26,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
-	github.com/googleapis/gax-go/v2 v2.6.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/imkira/go-interpol v1.0.0 // indirect
 	github.com/klauspost/compress v1.12.2 // indirect
@@ -49,7 +47,7 @@ require (
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
--- model/image_test.go
+++ model/image_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"google.golang.org/api/drive/v3"
 	"my-imgur/lib/upload_client"
 )
 
@@ -15,49 +16,12 @@ var _ = Describe("Image", func() {
 			image := Image{
 				uploadClient: client,
 			}
+			file := drive.File{
+				Name: "( ).jpeg",
+			}
 			client.EXPECT().UploadFile(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
 				Return(upload_client.UploadFileResponse{
-					PCloud: upload_client.PCloudUploadFileResponse{
-						Metadata: []struct {
-							Ismine         bool    `json:"ismine"`
-							Id             string  `json:"id"`
-							Created        string  `json:"created"`
-							Modified       string  `json:"modified"`
-							Hash           float64 `json:"hash"`
-							Isshared       bool    `json:"isshared"`
-							Isfolder       bool    `json:"isfolder"`
-							Category       int     `json:"category"`
-							Parentfolderid int     `json:"parentfolderid"`
-							Icon           string  `json:"icon"`
-							Fileid         int     `json:"fileid"`
-							Height         int     `json:"height"`
-							Width          int     `json:"width"`
-							Path           string  `json:"path"`
-							Name           string  `json:"name"`
-							Contenttype    string  `json:"contenttype"`
-							Size           int     `json:"size"`
-							Thumb          bool    `json:"thumb"`
-						}{struct {
-							Ismine         bool    `json:"ismine"`
-							Id             string  `json:"id"`
-							Created        string  `json:"created"`
-							Modified       string  `json:"modified"`
-							Hash           float64 `json:"hash"`
-							Isshared       bool    `json:"isshared"`
-							Isfolder       bool    `json:"isfolder"`
-							Category       int     `json:"category"`
-							Parentfolderid int     `json:"parentfolderid"`
-							Icon           string  `json:"icon"`
-							Fileid         int     `json:"fileid"`
-							Height         int     `json:"height"`
-							Width          int     `json:"width"`
-							Path           string  `json:"path"`
-							Name           string  `json:"name"`
-							Contenttype    string  `json:"contenttype"`
-							Size           int     `json:"size"`
-							Thumb          bool    `json:"thumb"`
-						}{Name: "( ).jpeg"}},
-					},
+					GoogleDriveApi: &file,
 				}, nil)
 			path, err := image.UploadFile("( ).jpeg", []byte{})
 			Expect(err).ShouldNot(HaveOccurred())
